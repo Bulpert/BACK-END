61a469c82a0564da98df081e9016d266d48f415b
Bulpert <bulpert@icloud.com>
2022-01-15 22:02:49 -0300

    First commit, change folder

---
+++ PHP/Introdução/Echo e Print.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Echo e Print</title>
+</head>
+
+<body>
+	
+	
+	<?php
+	
+	
+	echo 'Mostrando com echo<br>';
+	
+	
+	// Também podemos visualizar conteúdo na tela através da função print, quando ele exibe corretamente ele nos retorna o valor 1
+	
+	echo print('Exibindo com print')
+	
+	?>
+	
+</body>
+</html>
---
+++ PHP/Introdução/Hello World.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+
+<body>
+	<?php
+	
+	
+	echo 'Hello World';
+	?>
+</body>
+</html>
